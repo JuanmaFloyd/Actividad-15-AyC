e2fd513e9559f06332139d1f74fcadb53f2da7d2
Stefano <stefano_canossini@hotmail.com>
2019-06-13 17:22:38 -0300

    node

--- src/disjointSets/DisjointSets.java
+++ src/disjointSets/DisjointSets.java
@@ -1,5 +1,7 @@
 package disjointSets;
 
+import Graph.Graph;;
+
 public class DisjointSets {
 		
 	private Node[] nodes;
@@ -9,43 +11,31 @@ public class DisjointSets {
 		for (int i = 0; i < numElems; i++) {
 			Node node = new Node();
 			node.parent = node;
-			node.rank = 0;
 			nodes[i] = node;
 		}
 	}
-			private Node find(int elemIndex) {
-		if (elemIndex < 0 || elemIndex >= nodes.length)
-			throw new IndexOutOfBoundsException();
-		return find(nodes[elemIndex]);
-	}
-	
-	
+		
 	private static Node find(Node node) {
 		if (node.parent != node)
-			node.parent = find(node.parent);
+			find(node.parent);
 		return node.parent;
 	}
-	
 	
 	public boolean inSameSet(int elemIndex0, int elemIndex1) {
 		return find(elemIndex0) == find(elemIndex1);
 	}
 	
+	private Node find(int elemIndex) {
+		return find(nodes[elemIndex]);
+	}
 	
 	public boolean union(int elemIndex0, int elemIndex1) {
 		Node repr0 = find(elemIndex0);
-		Node repr1 = find(elemIndex1);
+		Node repr1 = find(elemIndex1);		
 		if (repr0 == repr1)
-			return false;
+			return false;		
+		repr1.parent = repr0;
 		
-		if (repr0.rank > repr1.rank)
-			repr1.parent = repr0;
-		else if (repr1.rank > repr0.rank)
-			repr0.parent = repr1;
-		else {
-			repr1.parent = repr0;
-			repr0.rank++;
-		}
 		return true;
 	}
 }
